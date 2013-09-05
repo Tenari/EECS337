f74d32f53432878fe2a5c1f285a9f2fe6955c781
Tenari <djz24@case.edu>
2013-09-05 17:28:44 -0400

    HW02: finished part4

---
+++ djz24/hw02/Code_1_6_4.cpp
@@ -0,0 +1,12 @@
+# 1 "Code_1_6_4.c"
+# 1 "<built-in>"
+# 1 "<command-line>"
+# 1 "Code_1_6_4.c"
+# 18 "Code_1_6_4.c"
+int x = 2;
+
+void b() { x = (x+1); printf ("%d\n", x) ; }
+
+void c() { int x = 1; printf ("%d\n", (x+1)); }
+
+int main() { b(); c(); }
