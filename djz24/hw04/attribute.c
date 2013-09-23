e0a7cfe268cd5382b557def7b16c556b2a2c17bf
Tenari <djz24@case.edu>
2013-09-23 13:25:00 -0400

    HW04: think I finished part 3

--- djz24/hw04/attribute.c
+++ djz24/hw04/attribute.c
@@ -4,7 +4,7 @@
 *
 * DESC:		EECS 337 Assignment 4
 *
-* AUTHOR:	caseid
+* AUTHOR:	djz24
 *
 * DATE:		September 17, 2013
 *
@@ -22,13 +22,50 @@
  */
 void	print_attribute( int index)
 {
+  ATTRIBUTE curr = data.attributes[index];
+  if (curr.token == IDENTIFIER){
+    fprintf( stdout, "token: IDENTIFIER\tbuffer: %s\tlength: %d\tformat: NONE\n", 
+                     curr.buffer, curr.length);
+  } else if (curr.token == STRING_LITERAL){
+    fprintf( stdout, "token: STRING_LITERAL\tbuffer: %s\tlength: %d\tformat: NONE\n", 
+                     curr.buffer, curr.length);
+  } else if (curr.token == CONSTANT){
+    switch (curr.format){
+      case FORMAT_CHAR:
+        fprintf( stdout, "token: CONSTANT\tbuffer: %s\tlength: %d\tformat: CHAR\t char:%s\n", 
+                         curr.buffer, curr.length, curr.buffer);
+        break;
+      case FORMAT_DECIMAL:
+        fprintf( stdout, "token: CONSTANT\tbuffer: %s\tlength: %d\tformat: DECIMAL\t decimal:%s\n", 
+                         curr.buffer, curr.length, curr.buffer);
+        break;
+      case FORMAT_HEXADECIMAL:
+        fprintf( stdout, "token: CONSTANT\tbuffer: %s\tlength: %d\tformat: HEXADECIMAL\t hex:%s\n", 
+                         curr.buffer, curr.length, curr.buffer);
+        break;
+      case FORMAT_OCTAL:
+        fprintf( stdout, "token: CONSTANT\tbuffer: %s\tlength: %d\tformat: OCTAL\t octal:%s\n", 
+                         curr.buffer, curr.length, curr.buffer);
+        break;
+      case FORMAT_FLOAT:
+        fprintf( stdout, "token: CONSTANT\tbuffer: %s\tlength: %d\tformat: FLOAT\t float:%s\n", 
+                         curr.buffer, curr.length, curr.buffer);
+        break;
+      default:
+        break;
+    }
+  }
 	return;
 }
 /*
- *	print the attriube table
+ *	print the attribute table
  */
 void	print_attribute_table( void)
 {
+  int i;
+  for(i = 0; i < data.index; i++){
+    print_attribute( i);
+  }
 	return;
 }
 #endif
@@ -42,9 +79,20 @@ int	attribute( int token, char *buffer, unsigned int length, int format)
 /*
  *	find the same string and return the index
  */
+  int i;
+  for (i = 0; i < MAX_ATTRIBUTES; i++){
+    if( !strncmp( buffer, data.attributes[i].buffer, length)){
+      return i;
+    }
+  }
 /*
  *	else update to the next attribute field
  */
+ data.index++;
+ data.attributes[data.index].token = token;
+ strcpy( data.attributes[data.index].buffer, buffer);
+ data.attributes[data.index].length = length;
+ data.attributes[data.index].format = format;
 /*
  *	encode the constant string into a value
  */
